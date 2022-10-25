b9143b4028d534dc7bf0dfff660316dcfc5c3901
ARIADNA <rariadna@inbox.ru>
2022-10-25 21:18:03 +0300

    Initial commit

---
+++ Задача 19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+void PalindromicNumber(int number)
+{
+    int num1 = number/10000;
+    int num2 = number%10;
+    if(num1 == num2)
+    {
+        number = number/10;
+        int num3 = (number/100)%10;
+        int num4 = number%10; 
+        if(num3 == num4)
+        {
+            Console.Write("Да");
+        }    
+    }
+    else 
+    {
+        Console.Write("Нет");
+    }
+}
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+PalindromicNumber(number); 
---
+++ Задача 23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class MyProgram
+{
+
+    static string getThirdDegree(int N)
+    {
+        string result = "";
+        for (int i = 1; i < N; i++)
+        {
+            result += (i * i * i).ToString() + ", ";
+        }
+        result += (N * N * N).ToString();
+        return result;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите N:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{n} -> {getThirdDegree(n)}");
+
+    }
+}
